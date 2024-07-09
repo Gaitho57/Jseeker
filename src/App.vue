86e90d8cd8d2891a7714f738f97705d3eb5ba5aa
Gaitho57 <michaelgaitho10@gmail.com>
2024-07-09 14:26:18 +0300

    [feat]: landing page

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import HeroSection from './components/HeroSection.vue'
+import FeatureBase from './components/FeatureBase.vue'
+</script>
+
+<template>
+  <HeroSection />
+  <FeatureBase />
+</template>
+
+<style scoped>
+.min-h-screen {
+  min-height: 100vh;
+}
+
+body {
+  margin: 0;
+}
+</style>
